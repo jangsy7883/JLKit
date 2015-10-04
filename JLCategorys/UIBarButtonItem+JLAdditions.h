03b65ed088819f8a793dd626c7f89b4607e0dcb8
jangsy7883 <jangsy7883@malangstudio.com>
2015-10-04 17:45:25 +0900

    정리.

--- JLCategorys/UIBarButtonItem+JLAdditions.h
+++ JLCategorys/UIBarButtonItem+JLAdditions.h
@@ -10,6 +10,13 @@
 
 @interface UIBarButtonItem (Additions)
 
-+ (UIBarButtonItem*)barButtonItemWithtButtonImage:(UIImage *)image target:(id)target action:(SEL)action;
++ (UIBarButtonItem*)barButtonItemWithtImage:(UIImage *)image
+                                      style:(UIBarButtonItemStyle)style
+                                     target:(id)target
+                                     action:(SEL)action;
+
++ (UIBarButtonItem*)barButtonItemWithtImage:(UIImage *)image
+                                     target:(id)target
+                                     action:(SEL)action;
 
 @end
--- JLCategorys/UIBarButtonItem+JLAdditions.m
+++ JLCategorys/UIBarButtonItem+JLAdditions.m
@@ -10,14 +10,21 @@
 
 @implementation UIBarButtonItem (Additions)
 
-+ (UIBarButtonItem*)barButtonItemWithtButtonImage:(UIImage *)image target:(id)target action:(SEL)action
++ (UIBarButtonItem*)barButtonItemWithtImage:(UIImage *)image style:(UIBarButtonItemStyle)style target:(id)target action:(SEL)action
 {
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setImage:image forState:UIControlStateNormal];
-    [button addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
-    [button sizeToFit];
-
-    return [[UIBarButtonItem alloc] initWithCustomView:button];
+    return [[UIBarButtonItem alloc] initWithImage:image
+                                            style:style
+                                           target:target
+                                           action:action];
 }
+
++ (UIBarButtonItem*)barButtonItemWithtImage:(UIImage *)image target:(id)target action:(SEL)action
+{
+    return [[UIBarButtonItem alloc] initWithImage:image
+                                            style:UIBarButtonItemStylePlain
+                                           target:target
+                                           action:action];
+}
+
 
 @end
