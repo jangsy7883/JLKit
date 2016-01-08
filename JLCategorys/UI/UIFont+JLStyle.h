133cc7e888194cc9a0b2442cb97e6a654fa47f98
jangsy7883 <jangsy7883@malangstudio.com>
2016-01-08 18:12:03 +0900

    #define 추가.

--- JLCategorys/UI/UIFont+JLStyle.h
+++ JLCategorys/UI/UIFont+JLStyle.h
@@ -18,7 +18,13 @@ typedef NS_ENUM(unsigned int, UIFontStyle)
     UIFontStyleHeavy,
 };
 
-#define SystemFont(fontSize,fontStyle) [UIFont systemFontOfSize:fontSize style:fontStyle]
+#define Font(fontSize,fontStyle) [UIFont systemFontOfSize:fontSize style:fontStyle]
+
+#define Font_Regular(fontSize) [UIFont systemFontOfSize:fontSize style:UIFontStyleRegular]
+#define Font_Light(fontSize) [UIFont systemFontOfSize:fontSize style:UIFontStyleLight]
+#define Font_Medium(fontSize) [UIFont systemFontOfSize:fontSize style:UIFontStyleMedium]
+#define Font_SemiBold(fontSize) [UIFont systemFontOfSize:fontSize style:UIFontStyleSemiBold]
+#define Font_Bold(fontSize) [UIFont systemFontOfSize:fontSize style:UIFontStyleBold]
 
 @interface UIFont (JLStyle)
 
